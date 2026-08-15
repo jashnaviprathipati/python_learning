818a2de2d6d2cb8c4f1c710d000f2adc8c3eb932
Jashnavi Prathipati <YOUR-GITHUB-EMAIL>
2026-08-15 15:05:45 +0530

    Add Day01 Python file

---
+++ Day01.py
@@ -0,0 +1,19 @@
+# Variable
+name = 'Jashnavi'
+# Multiple assignment
+a, b = 10, 20
+#Same value
+x = y = 100
+#Reassignment 
+age = 20
+age = 21
+#Swapping
+a, b = b, a
+#Delete
+del age
+#Single-line comment
+'''
+Multi
+line 
+comment
+'''
